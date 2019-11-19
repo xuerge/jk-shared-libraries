c04515d479f2b88aa34ece1da770caaccf2d3908
shiyi.xu <shiyi.xu@56qq.com>
2019-11-19 18:40:30 +0800

    no message

---
+++ src/com/xuerge/jk/Blog.groovy
@@ -0,0 +1,18 @@
+package com.xuerge.jk
+
+public class Blog implements Serializable {
+    def script
+    def work_dir="/root/blog"
+
+    Blog(Object script) {
+        this.script = script
+    }
+
+    def run() {
+        script.node {
+            script.echo "script.credentials('test')"
+            script.sh "ssh -o StrictHostKeyChecking=no root@47.52.174.68 cd $work_dir\\ git pull\\ docker-compose restart -d"
+        }
+    }
+}
+
