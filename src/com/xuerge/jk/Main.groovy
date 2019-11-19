f92dd7f75b2443aca0c01972e0c71b002d20b7bd
shiyi.xu <shiyi.xu@56qq.com>
2019-11-19 18:33:11 +0800

    no message

--- src/com/xuerge/jk/Main.groovy
+++ src/com/xuerge/jk/Main.groovy
@@ -13,7 +13,9 @@ public class Main implements Serializable {
     Main(Object script, String env) {
         this.script = script
         this.env = env
-        result = []
+        result = {
+            commit_id:''
+        }
 
     }
 
