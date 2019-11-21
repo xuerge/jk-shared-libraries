e294750147c9a69d5754af0ca44b65418e179d57
徐士义 <>
2019-11-22 00:22:12 +0800

    mend

--- src/com/xuerge/jk/Blog.groovy
+++ src/com/xuerge/jk/Blog.groovy
@@ -11,7 +11,7 @@ public class Blog implements Serializable {
     def run() {
         script.node {
             script.echo "${script.credentials('test')}"
-            script.sh "ssh -o StrictHostKeyChecking=no root@47.52.174.68 echo $(pwd)"
+            script.sh "ssh -o StrictHostKeyChecking=no root@47.52.174.68 cd /root/blog;restart"
         }
     }
 }
